496679742a5d41f377b07d4c769a41cdc40f87fb
Sina Rüeger <sina.rueeger@gmail.com>
2018-11-23 12:05:37 +0100

    improved examples, added gwas utils

---
+++ dev/geom_manhattan_dev.R
@@ -0,0 +1,7 @@
+
+
+## real distance
+## dat <- gwasResults %>% arrange(CHR, BP) %>% mutate(tmp = diff from start, x = cumsum(tmp))
+
+## new x axis
+## med.dat <- data %>% group_by(group) %>% summarise(median.x = median(cumsum.tmp))
